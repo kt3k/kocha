37612fe184e8ab9391a35e2153577c16586fcee4
Yoshiya Hinosawa <stibium121@gmail.com>
2017-06-04 23:23:56 +0900

    refactor: create utils/color module

--- src/reporters/base.js
+++ src/reporters/base.js
@@ -2,7 +2,8 @@ const tty = require('tty')
 const diff = require('diff')
 const ms = require('ms')
 const stringify = require('stringifier').stringify
-const supportsColor = process.browser ? null : require('supports-color')
+const color = require('../utils/color')
+const { useColors, colors, colorLines } = color
 
 /**
  * Save timer references to avoid Sinon interfering.
@@ -22,36 +23,6 @@ const clearInterval = global.clearInterval
 const isatty = tty.isatty(1) && tty.isatty(2)
 
 /**
- * Enable coloring by default, except in the browser interface.
- */
-const useColors = !process.browser && (supportsColor || (process.env.MOCHA_COLORS !== undefined))
-
-/**
- * Default color map.
- */
-const colors = {
-  pass: 90,
-  fail: 31,
-  'bright pass': 92,
-  'bright fail': 91,
-  'bright yellow': 93,
-  pending: 36,
-  suite: 0,
-  'error title': 0,
-  'error message': 31,
-  'error stack': 90,
-  checkmark: 32,
-  fast: 90,
-  medium: 33,
-  slow: 31,
-  green: 32,
-  light: 90,
-  'diff gutter': 90,
-  'diff added': 32,
-  'diff removed': 31
-}
-
-/**
  * Default symbol map.
  */
 const symbols = {
@@ -67,24 +38,6 @@ if (process.platform === 'win32') {
   symbols.ok = '\u221A'
   symbols.err = '\u00D7'
   symbols.dot = '.'
-}
-
-/**
- * Color `str` with the given `type`,
- * allowing colors to be disabled,
- * as well as user-defined color
- * schemes.
- *
- * @param {string} type
- * @param {string} str
- * @return {string}
- * @api private
- */
-const color = function (type, str) {
-  if (!exports.useColors) {
-    return String(str)
-  }
-  return '\u001b[' + exports.colors[type] + 'm' + str + '\u001b[0m'
 }
 
 /**
@@ -326,15 +279,6 @@ function unifiedDiff (err) {
     '\n\n' +
     lines.map(cleanUp).filter(notBlank).join('\n')
 }
-
-/**
- * Color lines for `str`, using the color `name`.
- * @api private
- * @param {string} name
- * @param {string} str
- * @return {string}
- */
-const colorLines = (name, str) => str.split('\n').map(str => color(name, str)).join('\n')
 
 /**
  * Object#toString reference.
---
+++ src/utils/color.js
@@ -0,0 +1,61 @@
+/**
+ * Enable coloring by default, except in the browser interface.
+ */
+const useColors = !process.browser && require('supports-color')
+
+/**
+ * Default color map.
+ */
+const colors = {
+  pass: 90,
+  fail: 31,
+  'bright pass': 92,
+  'bright fail': 91,
+  'bright yellow': 93,
+  pending: 36,
+  suite: 0,
+  'error title': 0,
+  'error message': 31,
+  'error stack': 90,
+  checkmark: 32,
+  fast: 90,
+  medium: 33,
+  slow: 31,
+  green: 32,
+  light: 90,
+  'diff gutter': 90,
+  'diff added': 32,
+  'diff removed': 31
+}
+
+/**
+ * Color `str` with the given `type`,
+ * allowing colors to be disabled,
+ * as well as user-defined color
+ * schemes.
+ *
+ * @param {string} type
+ * @param {string} str
+ * @return {string}
+ * @api private
+ */
+const color = function (type, str) {
+  if (!exports.useColors) {
+    return String(str)
+  }
+  return '\u001b[' + exports.colors[type] + 'm' + str + '\u001b[0m'
+}
+
+/**
+ * Color lines for `str`, using the color `name`.
+ * @api private
+ * @param {string} name
+ * @param {string} str
+ * @return {string}
+ */
+const colorLines = (name, str) => str.split('\n').map(str => color(name, str)).join('\n')
+
+exports = module.exports = color
+exports.useColors = useColors
+exports.colors = colors
+exports.colorLines = colorLines
