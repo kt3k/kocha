fade8fe2bdd4ffe2d95439fc4b833cd0d48ea7c0
Yoshiya Hinosawa <stibium121@gmail.com>
2017-06-04 19:35:06 +0900

    test: add test of cli

---
+++ src/__tests__/cli.js
@@ -0,0 +1,23 @@
+const { execSync } = require('child_process')
+const assert = require('power-assert')
+
+describe('cli', () => {
+  describe('-h, --help option', () => {
+    it('shows the help message', () => {
+      const output = execSync('./bin/kocha.js -h')
+      assert(/Usage: kocha \[options\] <files, \.\.\.>/.test(output))
+    })
+  })
+  describe('-v, --version', () => {
+    it('shows the version number', () => {
+      const output = execSync('./bin/kocha.js -v')
+      assert(/kocha@\d+\.\d+\.\d/.test(output))
+    })
+  })
+
+  it('throws when the input files are not given', () => {
+    assert.throws(() => {
+      execSync('./bin/kocha.js')
+    }, Error)
+  })
+})
