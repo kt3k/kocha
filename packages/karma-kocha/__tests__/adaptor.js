e67cae9e8b023a30602a61c06026166acb4dbfe2
Yoshiya Hinosawa <stibium121@gmail.com>
2017-06-10 01:11:31 +0900

    feat(karma-kocha): support ie11

--- packages/karma-kocha/__tests__/adaptor.js
+++ packages/karma-kocha/__tests__/adaptor.js
@@ -1,10 +1,12 @@
-const { describe, it } = require('../../../packages/kocha')
+const kocha = require('../../../packages/kocha')
+const describe = kocha.describe
+const it = kocha.it
 const assert = require('power-assert')
 
-const add = (a, b) => a + b
+const add = function (a, b) { return a + b }
 
-describe('add', () => {
-  it('12 + 13 = 25', () => {
+describe('add', function () {
+  it('12 + 13 = 25', function () {
     const sum = add(12, 13)
 
     assert.equal(sum, 25)
--- packages/karma-kocha/index.js
+++ packages/karma-kocha/index.js
@@ -5,6 +5,7 @@ var createPattern = function (path) {
 }
 var initKocha = function (files) {
   files.unshift(createPattern(path.join(__dirname, 'adaptor.js')))
+  files.unshift(createPattern(path.join(__dirname, 'es6-promise.auto.min.js')))
 }
 
 initKocha.$inject = ['config.files']
