b9f514d2bd2ff55555fb91874c5e0f181f55bc04
Yoshiya Hinosawa <stibium121@gmail.com>
2017-06-10 11:23:57 +0900

    test: run karma-kocha test on sauce labs in CI env

--- karma.conf.js
+++ karma.conf.js
@@ -1,4 +1,4 @@
-module.exports = config => config.set({
+const options = {
   frameworks: ['kocha', 'browserify'],
   files: [
     'packages/karma-kocha/__tests__/*.js'
@@ -6,9 +6,8 @@ module.exports = config => config.set({
   preprocessors: {
     'packages/karma-kocha/__tests__/*.js': ['browserify']
   },
-  browserify: {
-    debug: true
-  },
+  browserify: { debug: true },
+  reporters: ['progress'],
   browsers: ['Chrome'],
   singleRun: true,
   plugins: [
@@ -16,4 +15,16 @@ module.exports = config => config.set({
     'karma-browserify',
     'karma-chrome-launcher'
   ]
-})
+}
+
+if (process.env.CI) {
+  // Sauce Labs settings
+  const customLaunchers = require('./karma.custom-launchers.js')
+  options.plugins.push('karma-sauce-launcher')
+  options.customLaunchers = customLaunchers
+  options.browsers = Object.keys(options.customLaunchers)
+  options.reporters.push('saucelabs')
+  options.sauceLabs = { testName: 'kocha-ci' }
+}
+
+module.exports = config => config.set(options)
